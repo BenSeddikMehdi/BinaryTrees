b296704a904735a4f8f1890fe5e5b016788ffa34
BenSeddikMehdi <amigoinpt@gmail.com>
2019-12-13 17:10:15 +0100

    Creating Trees by using While loop function

--- Trees.h
+++ Trees.h
@@ -2,7 +2,60 @@
 // Created by El Mehdi Benseddik on 12/12/2019.
 //
 
+#include <stdint.h>
+#include <stdlib.h>
+
 #ifndef BINARYTREES_TREES_H
 #define BINARYTREES_TREES_H
+
+typedef struct p_node {
+    int16_t data;
+    struct p_node* left;
+    struct p_node* right;
+} node_t;
+
+node_t* do_create_node(node_t* (*op) (node_t**, int16_t), node_t** pNode, int16_t value) {
+    return op(pNode, value);
+}
+node_t* new_node(node_t* node, int value) {
+    node = malloc(sizeof(node_t));
+    node->data = value;
+    node->left = NULL;
+    node->right = NULL;
+    return node;
+}
+node_t* create_node(node_t** pNode, int16_t value) {
+    if ((*pNode) != NULL) {
+        while ((*pNode) != NULL) {
+            if (value < (*pNode)->data) {
+                if ((*pNode)->left != NULL)
+                    (*pNode) = (*pNode)->left;
+                else {
+                    (*pNode)->left = new_node((*pNode)->left, value);
+                    return (*pNode)->left;
+                }
+
+            }
+            else {
+                if ((*pNode)->right != NULL)
+                    (*pNode) = (*pNode)->right;
+                else {
+                    (*pNode)->right = new_node((*pNode)->right, value);
+                    return (*pNode)->right;
+                }
+            }
+        }
+    } else
+        (*pNode) = new_node((*pNode), value);
+    return (*pNode);
+}
+void printData(node_t* p_node) {
+    if (p_node == NULL) return;
+    if (p_node->left != NULL) printData(p_node->left);
+    printf("%d\n", p_node->data);
+    if (p_node->right != NULL) printData(p_node->right);
+}
+
+
 
 #endif //BINARYTREES_TREES_H
--- main.c
+++ main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-#include "binaryTrees.h"
+#include "Trees.h"
 
 int main() {
-    BinaryNode_t* treeBranches = malloc(sizeof(BinaryNode_t));
+/*    BinaryNode_t* treeBranches = malloc(sizeof(BinaryNode_t));
     BinaryNode_t* temporaryNode = NULL;
     treeBranches->data = 0;
     treeBranches->left = NULL;
@@ -11,13 +11,19 @@ int main() {
     insertNewData(treeBranches, 9);
     //temporaryNode = returnNode(treeBranches, 6);
     printDataInOrder(treeBranches);
-/*
+*//*
     insertNewData(treeBranches, 9);
     //a = searchValue(treeBranches, 7);
 
     printf("\nPrinting Data: \n");
     printDataInOrder(temporaryNode);
-    printf("find the value of 6 at the line Node %d\n", temporaryNode->data);*/
-    free(treeBranches);
+    printf("find the value of 6 at the line Node %d\n", temporaryNode->data);*//*
+    free(treeBranches);*/
+    node_t* firstNode = NULL;
+    do_create_node(create_node, &firstNode, 5);
+    do_create_node(create_node, &firstNode, 6);
+    do_create_node(create_node, &firstNode, 7);
+    printData(firstNode);
+
     return 0;
 }
